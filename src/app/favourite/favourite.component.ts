c1e4f476f0cefbf310bb20827fd87adf22e7ab35
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-12 22:41:00 +0530

    favourite page create
    now when users go to favourite section it will show their favourite posts

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import _ from 'lodash';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-favourite',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favourite.component.css']
 })
 export class FavouriteComponent implements OnInit {
+  favoritesList: any = [];
 
   constructor() { }
 
   ngOnInit() {
+
+    const uid = firebase.auth().currentUser.uid;
+    const favRef = firebase.database().ref('favourites').child(uid);
+
+    favRef.once('value').then(snapshot => {
+      const favouritesObj = snapshot.val();
+
+      this.favoritesList = _.values(favouritesObj);
+
+    });
   }
 
 }
