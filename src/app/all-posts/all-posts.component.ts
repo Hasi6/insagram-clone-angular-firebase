e2274c732f9f8b0bb5ff7245293b42129bdbcc8d
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-12 17:39:34 +0530

    Update all-posts.component.ts
    Fix load more button error

--- src/app/all-posts/all-posts.component.ts
+++ src/app/all-posts/all-posts.component.ts
@@ -10,42 +10,41 @@ import _ from 'lodash';
 export class AllPostsComponent implements OnInit, OnDestroy {
   allRef: any;
   loadMoreRef: any;
-  limit = 4;
   all: any = [];
 
   constructor() { }
   onLoadMore() {
 
-    this.limit += 4;
-    this.allRef = firebase.database().ref('allposts').limitToFirst(this.limit);
-    this.allRef.on('child_added', data => {
-      this.all.push({
-        key: data.key,
-        data: data.val()
-      });
-    });
-
-    // if (this.all.length > 1 ) {
-    //   const lastLoadedPost = _.last(this.all);
-    //   const lastLoadedPostKey = lastLoadedPost.key;
-    //
-    //   this.loadMoreRef = firebase.database().ref('allposts').startAt(lastLoadedPostKey, lastLoadedPost).limitToFirst(7);
-    //
-    //   this.loadMoreRef.on('child_added', data => {
-    //
-    //     if ( data.key === lastLoadedPostKey ) {
-    //       return;
-    //     } else {
-    //       this.all.push({
-    //         key: data.key,
-    //         data: data.val()
-    //       });
-    //     }
+    // this.limit += 4;
+    // this.allRef = firebase.database().ref('allposts').limitToFirst(this.limit);
+    // this.allRef.on('child_added', data => {
+    //   this.all.push({
+    //     key: data.key,
+    //     data: data.val()
     //   });
-    // }
+    // });
+
+    if (this.all.length > 1 ) {
+      const lastLoadedPost = _.last(this.all);
+      const lastLoadedPostKey = lastLoadedPost.key;
+
+      this.loadMoreRef = firebase.database().ref('allposts').orderByKey().startAt(lastLoadedPostKey).limitToFirst(12);
+
+      this.loadMoreRef.on('child_added', data => {
+
+        if ( data.key === lastLoadedPostKey ) {
+          return;
+        } else {
+          this.all.push({
+            key: data.key,
+            data: data.val()
+          });
+        }
+      });
+    }
   }
   ngOnInit() {
-    this.allRef = firebase.database().ref('allposts').limitToFirst(this.limit);
+    this.allRef = firebase.database().ref('allposts').limitToFirst(12);
     this.allRef.on('child_added', data => {
       this.all.push({
         key: data.key,
